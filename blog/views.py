2365c4af936c5827380e70b3f16b8d8e7d5c9d3d
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-06 09:28:31 +0530

    added templates in blog app and passed context from views to template pages

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#post array of dictionary
+
+posts = [
+    {
+        'name': 'dhruv singhal',
+        'title': 'time management',
+        'body': 'dm me for effective time management strategies',
+        'date_published' : '2nd Jan, 2021'
+    },
+    {
+        'name': 'aayush singhal',
+        'title': 'JEE preparation',
+        'body': 'dm me for effective time JEE strategies',
+        'date_published' : '8th Jan, 2021'
+    }
+]
+
 def home(request):
-    return render(request, 'blog-home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'blog/about.html')
