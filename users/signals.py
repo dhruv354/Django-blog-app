e9471c7965c8c9b2599000cc08aab953b1ea412a
dhruv354 <dhruvsin@iitk.ac.in>
2021-06-09 21:51:38 +0530

    fixed some syntax errors

--- users/signals.py
+++ users/signals.py
@@ -7,12 +7,12 @@ from django.dispatch import receiver
 from .models import Profile
 
 
-@receiver(Signal=post_save, sender=User)
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(Signal=post_save, sender=User)
+@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
